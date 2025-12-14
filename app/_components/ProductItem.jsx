66e1f3c3e827b4bf1fedb791b5ee069feabea0cf
Abdullah <abdulhhany69@gmail.com>
2025-12-14 06:42:59 +0200

    feat: convert ProductItem component to use Link for navigation to product details

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -1,10 +1,14 @@
 import { List } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ProductItem = ({ product }) => {
   return (
-    <div className="hover:border p-1 border-teal-400 rounded-lg hover:shadow-md hover:cursor-pointer">
+    <Link
+      href={`/product-details/${product.documentId}`}
+      className="hover:border p-1 border-teal-400 rounded-lg hover:shadow-md  "
+    >
       <Image
         src={product?.banner?.url}
         alt="banner-card"
@@ -14,7 +18,9 @@ const ProductItem = ({ product }) => {
       />
       <div className="flex justify-between items-center p-3 bg-gray-50 rounded-b-md">
         <div className="">
-          <h2 className="text-[14px] font-medium line-clamp-1">{product.title}</h2>
+          <h2 className="text-[14px] font-medium line-clamp-1">
+            {product.title}
+          </h2>
           <h2 className="text-[11px] text-gray-400 flex gap-2 items-center">
             <List className="w-4 h-4" />
             {product.category}
@@ -22,7 +28,7 @@ const ProductItem = ({ product }) => {
         </div>
         <h2>{product.price}</h2>
       </div>
-    </div>
+    </Link>
   );
 };
 
