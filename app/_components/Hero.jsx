c360f6e4bc4d6350ac974227407ee8329bd462c3
Abdullah <abdulhhany69@gmail.com>
2025-12-16 14:30:55 +0200

    feat: refactor Hero component for improved layout and styling

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,35 +1,41 @@
+import Link from "next/link";
 import React from "react";
 
-const Hero = () => {
+function Hero() {
   return (
-    <section className="bg-white lg:grid lg:h-screen  ">
-      <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
-        <div className="mx-auto max-w-prose text-center">
-          <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
+    <section className="bg-gray-50">
+      <div className="max-w-screen-xl px-4 py-32 mx-auto pt-36 lg:flex lg:h-screen">
+        <div className="max-w-xl mx-auto text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
             All Your Digital Products
-            <strong className="text-primary"> Is One Click Away </strong>
+            <strong className="font-extrabold text-primary sm:block">
+              Is One Click Away
+            </strong>
           </h1>
-          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+
+          <p className="mt-4 sm:text-xl/relaxed">
             Start Exploring State of the Art Assets Now!
           </p>
-          <div className="mt-4 flex justify-center gap-4 sm:mt-6">
-            <a
-              className="inline-block rounded border border-teal-600 bg-teal-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-teal-500"
-              href="#"
+
+          <div className="flex flex-wrap justify-center gap-4 mt-8">
+            <Link
+              className="block w-full px-12 py-3 text-sm font-medium text-white bg-teal-600 rounded shadow hover:bg-primary focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href=" "
             >
               Get Started
-            </a>
-            <a
-              className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
-              href="#"
+            </Link>
+
+            <Link
+              className="block w-full px-12 py-3 text-sm font-medium text-teal-600 rounded shadow hover:text-primary focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              href=" "
             >
               Learn More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default Hero;
