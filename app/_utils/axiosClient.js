715361c8524670be685c306376149f5c89f1b526
Abdullah <abdulhhany69@gmail.com>
2025-12-18 14:42:36 +0200

    chore: transfer Strapi data to Cloud, add .env variables, and configure Next.js image domains

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = " http://localhost:1337/api";
+const apiUrl = "https://incredible-fish-520a0182e4.strapiapp.com/api";
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
