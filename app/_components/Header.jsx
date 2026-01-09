475e424390632f753f4b99ec905a062e53aa1fcd
Abdullah <abdulhhany69@gmail.com>
2026-01-09 09:00:45 +0200

    Refactor Header component to conditionally hide on sign-in page and improve import order

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,23 +1,23 @@
 "use client";
-import { UserButton, useUser } from "@clerk/nextjs";
+import { useUser, UserButton } from "@clerk/nextjs";
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
-import React, { useContext, useEffect, useState, useRef } from "react";
+import Link from "next/link";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
 import { CartContext } from "../_context/CartContext";
 import CartApis from "../_utils/CartApis";
 import Cart from "./Cart";
-import Link from "next/link";
 
 const Header = () => {
+  const pathname = usePathname();
+  const hideHeaderRoutes = ["/sign-in"];
+  const shouldHideHeader = hideHeaderRoutes.includes(pathname);
+
   const cartRef = useRef(null);
-
   const { user } = useUser();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const [openCart, setOpenCart] = useState(false);
-
   const { cart, setCart } = useContext(CartContext);
-  // console.log("cart", cart);
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -25,145 +25,131 @@ const Header = () => {
         setOpenCart(false);
       }
     };
-
     document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   useEffect(() => {
-    setIsLoggedIn(window.location.href.toString().includes("sign-in"));
-  }, []);
-
-  useEffect(() => {
-    user && getCartItems();
+    if (user) getCartItems();
   }, [user]);
 
   const getCartItems = () => {
     CartApis.getUserCartItems(user.primaryEmailAddress.emailAddress).then(
       (res) => {
-        // console.log("response from cart items", res?.data?.data);
         res?.data?.data.forEach((citem) => {
-          // console.log("citem", citem);
-
           setCart((oldCart) => [
             ...oldCart,
-            {
-              id: citem.documentId,
-              product: citem?.products[0],
-            },
+            { id: citem.documentId, product: citem?.products[0] },
           ]);
         });
       }
     );
   };
 
+  if (shouldHideHeader) return null;
+
   return (
-    !isLoggedIn && (
-      <header className="bg-white  shadow-md">
-        <div className="mx-auto flex h-16 max-w-full items-center gap-8 px-4 sm:px-6 lg:px-14">
-          <Link href="/">
-            <Image src="/logo.svg" alt="logo" width={40} height={40} />
-          </Link>
-          <div className="flex flex-1 items-center justify-end md:justify-between">
-            <nav aria-label="Global" className="hidden md:block">
-              <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/"
-                  >
-                    Explore
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/"
-                  >
-                    Projects
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/"
-                  >
-                    About Us
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="/"
-                  >
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <div className="flex items-center gap-4">
-              {!user ? (
-                <div className="sm:flex sm:gap-4">
-                  <a
-                    className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-500"
-                    href="/sign-in"
-                  >
-                    Login
-                  </a>
-                  <a
-                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-500/75 sm:block"
-                    href="/sign-in"
-                  >
-                    Register
-                  </a>
-                </div>
-              ) : (
-                <div className="flex items-center gap-5">
-                  <h2 className="flex gap-1 cursor-pointer ">
-                    <ShoppingCart onClick={() => setOpenCart(!openCart)} />(
-                    {cart?.length})
-                  </h2>
-                  <UserButton />
-                  {openCart && (
-                    <div ref={cartRef}>
-                      <Cart />
-                    </div>
-                  )}
-                </div>
-              )}
-              <button className="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
+    <header className="bg-white shadow-md">
+      <div className="mx-auto flex h-16 max-w-full items-center gap-8 px-4 sm:px-6 lg:px-14">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={40} height={40} />
+        </Link>
+        <div className="flex flex-1 items-center justify-end md:justify-between">
+          <nav aria-label="Global" className="hidden md:block">
+            <ul className="flex items-center gap-6 text-sm">
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  Explore
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-500 transition hover:text-gray-500/75"
+                  href="/"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex items-center gap-4">
+            {!user ? (
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-500"
+                  href="/sign-in"
+                >
+                  Login
+                </a>
+                <a
+                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-500/75 sm:block"
+                  href="/sign-in"
+                >
+                  Register
+                </a>
+              </div>
+            ) : (
+              <div className="flex items-center gap-5">
+                <h2 className="flex gap-1 cursor-pointer">
+                  <ShoppingCart onClick={() => setOpenCart(!openCart)} />(
+                  {cart?.length})
+                </h2>
+                <UserButton />
+                {openCart && (
+                  <div ref={cartRef}>
+                    <Cart />
+                  </div>
+                )}
+              </div>
+            )}
+            <button className="block rounded-sm bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
         </div>
-      </header>
-    )
+      </div>
+    </header>
   );
 };
 
